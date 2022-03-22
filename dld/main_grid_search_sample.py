506b44725c6f66f6eaa487daf0050bb767f37ee3
D.Shimane <101233945+D-Shimane@users.noreply.github.com>
2022-03-22 16:30:36 +0900

    Update main_grid_search_sample.py

--- dld/main_grid_search_sample.py
+++ dld/main_grid_search_sample.py
@@ -1,4 +1,4 @@
-# グリッドサーチ実行の雛形
+# Template of executing Grid search
 from collections import OrderedDict
 import subprocess
 from subprocess import Popen
@@ -7,30 +7,30 @@ from grid_search import GridSearch
 variable_options = OrderedDict()
 fixed_options = OrderedDict()
 
-#-----[変更箇所 ここから]-----
-# 実行するファイル名を指定
+#-----[Changes from here]-----
+# Specify the name of the file to execute
 target_file = "main_eeg.py"
 
 save_dir_prefix = "./saved_eeg"
-# 保存ディレクトリは、"./saved_eeg_0_0", "./saved_eeg_0_1" ... などとなる.
+# The save directory should be "./saved_eeg_0_0", "./saved_eeg_0_1" ... and so on
 
 #save_dir_prefix = "./saved_eeg/gs"
-#などとサブディレクトリまでを指定すると
-# "./saved_eeg/gs_0_0", "./saved_eeg/gs_0_1" ... などディレクトリが作られる.
+# When specifying up to a subdirectory as above
+# The save directory will be "./saved_eeg/gs_0_0", "./saved_eeg/gs_0_1" ... and so on
 
-# グリッドサーチ対象オプション
-# 対処のオプションを配列で指定する
+# Target options of Grid search
+# Specify target options as an array
 variable_options["lr"]           = ["0.1", "0.01", "0.001"]
 variable_options["weight_decay"] = ["0.0", "0.01", "0.1"]
 
 
-# 固定オプション
+# Fixed options
 fixed_options["data_dir"]      = "./data2/DLD/Data_Converted"
 fixed_options["model_type"]    = "filter2"
 fixed_options["test_subjects"] = "TM_191008_01,TM_191009_01"
-fixed_options["fold_size"]     = "1" # 1Foldのみを対象にして高速化する場合
+fixed_options["fold_size"]     = "1" # When only 1 Fold is targeted to speed up the processing
 fixed_options["gpu"]           = "1"
-#-----[変更箇所 ここまで]-----
+#-----[To here]-----
 
 
 grid_search = GridSearch(target_file,
@@ -40,12 +40,12 @@ grid_search = GridSearch(target_file,
 
 command_lines = grid_search.get_command_lines()
 
-# グリッドサーチ実行
+# Execution of Grid search
 for command_line in command_lines:
     print("executing: {}".format(command_line))
     proc = Popen(command_line, shell=True)
     proc.wait()
 
-# グリッドサーチ結果サマリ出力
-# saved_eeg00_summary/summary.txt にサマリを出力する.
+# Output the summarized results of Grid search
+# to "saved_eeg00_summary/summary.txt"
 grid_search.export_summary()
